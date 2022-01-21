7710a2f247ecf7003829c054f3278b3dcd86720c
Илья <qq.ferveks@gmail.com>
2022-01-21 04:50:42 +0300

    fix

--- src/main/java/job4j/chat/control/PersonControl.java
+++ src/main/java/job4j/chat/control/PersonControl.java
@@ -2,7 +2,6 @@ package job4j.chat.control;
 
 import job4j.chat.entity.Person;
 import job4j.chat.service.PersonService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -14,11 +13,9 @@ import java.util.stream.StreamSupport;
 public class PersonControl {
 
     private final PersonService persons;
-    private final BCryptPasswordEncoder passwordEncoder;
 
-    public PersonControl(PersonService persons, BCryptPasswordEncoder passwordEncoder) {
+    public PersonControl(PersonService persons) {
         this.persons = persons;
-        this.passwordEncoder = passwordEncoder;
     }
 
     @GetMapping("/")
@@ -33,9 +30,8 @@ public class PersonControl {
         return persons.findById(id);
     }
 
-    @PostMapping("/sing-up")
+    @PostMapping("/")
     public Person savePerson(@RequestBody Person person) {
-        person.setPassword(passwordEncoder.encode(person.getPassword()));
         return persons.savePerson(person);
     }
 
@@ -48,4 +44,6 @@ public class PersonControl {
     public void deletePerson(@PathVariable int id) {
         persons.deletePerson(id);
     }
+
+
 }
